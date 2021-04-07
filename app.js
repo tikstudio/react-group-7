2af265c6c855c9019abd03e35a84b2cb94dd107f
Poxos <tigran.inbox@gmail.com>
2021-04-07 15:09:10 +0400

    removed jade

--- app.js
+++ app.js
@@ -10,9 +10,6 @@ import headers from './middlewares/headers';
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
